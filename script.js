05aeed362bff5ae5e05f0501986ecd9f13c8eb72
Md Shabrez Akhtar <77329187+Shabrez575@users.noreply.github.com>
2024-11-02 11:00:10 +0000

    CR1

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const sidemenu = document.querySelector("#sidemenu");
 
-function openMenu(){
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav");
+
+function openMenu() {
   sidemenu.style.transform = 'translateX(-16rem)';
 }
-function closeMenu(){
+function closeMenu() {
   sidemenu.style.transform = 'translateX(16rem)';
 }
+
+window.addEventListener('scroll', () => {
+  if (scrollY > 50) {
+    navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+    navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+  }
+  else {
+    navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+  }
+})
